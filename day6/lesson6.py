3d2ca687341c159060fb979aefc7385327be944b
Reif0 <siradzeluka115@mail.com>
2023-09-11 22:17:24 +0400

    commit day6

---
+++ day6/lesson6.py
@@ -0,0 +1,31 @@
+# for i in range(20):
+#     print(str(i) + "luka")
+
+# for i in range(1, 10, 2):
+#     print(str(i) + "luka")
+
+# i = 0
+# while i < 5:
+#     print("luka")
+#     print(i)
+#     i += 1
+
+# full_name ="luka siradze"
+# i = 0
+# while i  < len(full_name):
+#     print(full_name[i])
+#     print(i)
+#     i += 1
+
+
+# a = "qwwqw"
+# b = "bwbwb"
+
+# i = 0
+
+# while i < len(a):
+#     print(a[i]+b[i])
+#     i =+  1
+
+if 10 > 5 and 3 > 1:
+    print("cool")
