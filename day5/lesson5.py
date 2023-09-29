69981c464702e0e2ce264a68356d8ad5c3000004
Reif0 <siradzeluka115@mail.com>
2023-09-30 00:01:58 +0400

    commit day10

--- day5/lesson5.py
+++ day5/lesson5.py
@@ -1,17 +1,17 @@
-# num1 = int(input("enter num1: ")) 
-# num2 = int(input("enter num2: "))
-# num3 = int(input("enter num3: "))
+num1 = int(input("enter num1: ")) 
+num2 = int(input("enter num2: "))
+num3 = int(input("enter num3: "))
 
-# my_sum= 0
+my_sum= 0
 
-# if num1 % 2 == 1:
-#     my_sum += num1
-# if num2 % 2 == 1:
-#     my_sum += num2
-# if num3 % 2 == 1:
-#     my_sum += num3
+if num1 % 2 == 1:
+    my_sum += num1
+if num2 % 2 == 1:
+    my_sum += num2
+if num3 % 2 == 1:
+    my_sum += num3
 
-# print("the sum of add number is {}".format(my_sum))
+print("the sum of add number is {}".format(my_sum))
 
 # my_name = "luka"
 # for char in my_name:
@@ -48,23 +48,23 @@
 
 # print('there is {} amount of "a"  in this text'. format(amount_a_sentence))
 
-num1 = input("enter num1: ")
-num2 = input("enter num2: ")
+# num1 = input("enter num1: ")
+# num2 = input("enter num2: ")
 
-sum1 = 0
-sum2 = 0
+# sum1 = 0
+# sum2 = 0
 
 
-for char in num1:
-    if char == "bgdvztklmnpjrsqzxch":
-        sum1 += 1
-for char in num2:
-    if char == "bgdvztklmnpjrsqzxch":
-        sum2 += 1
-if num1 > num2:
-    print('am winadaebashi {} "xmovania"'. format(2))
-elif num1 < num2:
-    print('am winadaebashi {} "xmovania"'. format(2))
+# for char in num1:
+#     if char == "bgdvztklmnpjrsqzxch":
+#         sum1 += 1
+# for char in num2:
+#     if char == "bgdvztklmnpjrsqzxch":
+#         sum2 += 1
+# if num1 > num2:
+#     print('am winadaebashi {} "xmovania"'. format(2))
+# elif num1 < num2:
+#     print('am winadaebashi {} "xmovania"'. format(2))
 
 
 
