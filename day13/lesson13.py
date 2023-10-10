91d9bfbf4a6c400554689ee6e305b4baeb6e6eeb
Reif0 <siradzeluka115@mail.com>
2023-10-11 01:13:53 +0400

    commit day13

---
+++ day13/lesson13.py
@@ -0,0 +1,25 @@
+# home work 1
+arithmetic = (9+10+2+3) / 4
+if arithmetic > 9:
+    print("გილოცავ მატრიცელო შენ გადმოგეცა 300 ლარიანი ბანძი ტოსტერი რასაც შეალიე შენი ცხოვრების წლები")
+elif arithmetic < 5:
+    print("გილოცავ შენ გაექეცი მატრიცას")
+elif range (5,9):
+    print("you are mid")
+else:
+    arithmetic > 10 or arithmetic < 0
+    print("something is wrong whis you")
+
+# home work 2
+sallary = int(input("what is your sallary: "))
+if sallary > int(10000):
+    print("გოაში საწავლობდი?")
+elif sallary < int(1000):
+    print("შემოსულიყავი გოაში მატრიცელო")
+elif range (1000,10000):
+    print("you are mid")
+    
+
+
+
+
