23f77d7da10bb314f8a904530e4a6e637420e833
Reif0 <siradzeluka115@mail.com>
2023-10-18 02:26:35 +0400

    commit day15

--- day5/lesson5.py
+++ day5/lesson5.py
@@ -1,40 +1,40 @@
-num1 = int(input("enter num1: ")) 
-num2 = int(input("enter num2: "))
-num3 = int(input("enter num3: "))
+# num1 = int(input("enter num1: ")) 
+# num2 = int(input("enter num2: "))
+# num3 = int(input("enter num3: "))
 
-my_sum= 0
+# my_sum= 0
 
-if num1 % 2 == 1:
-    my_sum += num1
-if num2 % 2 == 1:
-    my_sum += num2
-if num3 % 2 == 1:
-    my_sum += num3
+# if num1 % 2 == 1:
+#     my_sum += num1
+# if num2 % 2 == 1:
+#     my_sum += num2
+# if num3 % 2 == 1:
+#     my_sum += num3
 
-print("the sum of add number is {}".format(my_sum))
+# print("the sum of add number is {}".format(my_sum))
 
-# my_name = "luka"
-# for char in my_name:
-#     print(char)
+my_name = "luka"
+for char in my_name:
+    print(char)
 
-# name1 = input("enter name1: ")
-# name2 = input("enter name2: ")
+name1 = input("enter name1: ")
+name2 = input("enter name2: ")
 
-# amount_of_vowels_in_name1 =  0
-# amount_of_vowels_in_name2 =  0
+amount_of_vowels_in_name1 =  0
+amount_of_vowels_in_name2 =  0
 
-# for char in name1:
-#     if char in "aeiou":
-#         amount_of_vowels_in_name1 += 1
+for char in name1:
+    if char in "aeiou":
+        amount_of_vowels_in_name1 += 1
 
-# for char in name2:
-#     if char in "aeiou":
-#         amount_of_vowels_in_name2 += 3
+for char in name2:
+    if char in "aeiou":
+        amount_of_vowels_in_name2 += 3
 
-# if amount_of_vowels_in_name1 > amount_of_vowels_in_name2:
-#     print("amount of wovels in name1 is less {} wovels".format(1))
-# elif amount_of_vowels_in_name2 > amount_of_vowels_in_name1:
-#     print("amount of wovels in name2 is more {} wovels".format(3))
+if amount_of_vowels_in_name1 > amount_of_vowels_in_name2:
+    print("amount of wovels in name1 is less {} wovels".format(1))
+elif amount_of_vowels_in_name2 > amount_of_vowels_in_name1:
+    print("amount of wovels in name2 is more {} wovels".format(3))
 
 
 #gamarjoba me mkvia luka
