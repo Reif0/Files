b956e11053ce3c7fd10b470fc82684d13517660a
Reif0 <siradzeluka115@mail.com>
2023-11-01 01:32:01 +0400

    commit code wars day1

---
+++ code wars day 1/lesson1.py
@@ -0,0 +1,12 @@
+# code wars home work 1
+# def repeat_str(repeat, string):
+#     return string * repeat
+
+# code wars home work 2
+# def remove_char(s):
+#     return s[1:-1]
+
+# code wars home work 3
+list = [1,30,55,38]
+print(min(list))
+
