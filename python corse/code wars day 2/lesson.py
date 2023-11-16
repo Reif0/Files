e0916561185ab14e4525f83a2f9f2d10ce7a1c49
Reif0 <144228216+Reif0@users.noreply.github.com>
2023-11-16 15:30:36 +0400

    Add files via upload

---
+++ python corse/code wars day 2/lesson.py
@@ -0,0 +1,21 @@
+# home work 1
+def square_sum (numbers):
+    list = []
+    for number in numbers:
+        number = number ** 2
+        list.append(number)
+    return sum(list)
+
+# home work 2
+def no_space(x):
+    z = x.replace(" ", "")
+    return z
+
+# home work 3
+def litres(time):
+    return time // 2
+
+# home work 4
+def greet():
+    return "hello world!"
+
