37149edc55c63dfe5e334ae59e124e190bdc0e4a
Reif0 <144228216+Reif0@users.noreply.github.com>
2024-03-22 09:58:14 -0700

    Delete java script/lesson1 directory

--- java script/lesson1/homework/script.js
+++
@@ -1,7 +0,0 @@
-let int = prompt("please enter your age")
-if (int < 18) {
-    console.log("u are under age")
-}else if(int > 18){
-    console.log("welcome")
-}else if(int == 18){
-    console.log("welcome")}
