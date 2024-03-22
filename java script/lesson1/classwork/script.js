d2b4a16795c3d4a614e102b1ba0d86abdad7fbe1
Reif0 <144228216+Reif0@users.noreply.github.com>
2024-03-22 20:57:19 +0400

    Add files via upload

---
+++ java script/lesson1/classwork/script.js
@@ -0,0 +1,5 @@
+let myName = "luka"
+let MyAge = 20
+const MyBirthDay = 2
+console.log("my name is" , myName , "and i am" , MyAge ,"and my birthday are in", MyBirthDay ,"june")
+console.log(myName.length)
---
+++ java script/lesson1/homework/script.js
@@ -0,0 +1,7 @@
+let int = prompt("please enter your age")
+if (int < 18) {
+    console.log("u are under age")
+}else if(int > 18){
+    console.log("welcome")
+}else if(int == 18){
+    console.log("welcome")}
