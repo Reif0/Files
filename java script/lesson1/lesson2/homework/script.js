0a1d9115c7e5a6126141d27f6269e547dc91c1ff
Reif0 <144228216+Reif0@users.noreply.github.com>
2024-04-02 10:55:27 -0700

    Delete java script/lesson1 directory

--- java script/lesson1/lesson2/homework/script.js
+++
@@ -1,9 +0,0 @@
-let num = prompt("please enter number")
-let num1 = 0
-if(num == num1){
-    console.log(num)
-}else if(num == +num){
-    console.log(num * -1)
-}else if(num == -num){
-    console.log(num *-1)
-}
--- java script/lesson1/lesson2/homework/script1.js
+++
@@ -1,6 +0,0 @@
-let arr = prompt("please enter your number")
-let sum = 0
-for(let i = 0; i < arr.length ; i++){
-    sum += arr[i]
-}
-console.log(sum)
