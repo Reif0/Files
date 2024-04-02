6ba31089cad85ecbbdcbfd3376f86aad7ae3fddc
Reif0 <144228216+Reif0@users.noreply.github.com>
2024-04-02 21:56:19 +0400

    Add files via upload

---
+++ java script/lesson2/homework/script.js
@@ -0,0 +1,9 @@
+let num = prompt("please enter number")
+let num1 = 0
+if(num == num1){
+    console.log(num)
+}else if(num == +num){
+    console.log(num * -1)
+}else if(num == -num){
+    console.log(num *-1)
+}
